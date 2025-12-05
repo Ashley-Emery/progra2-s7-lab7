a69267875bdaeb98b7741ef02494f0be23ef2ad9
Ashley Fajardo <a.e.fajardo06@gmail.com>
2025-12-05 09:19:51 -0600

    creacion de metodos reloadhashtable, adduser y deactivateuser

--- src/progra2/s7/lab7/PSNUsers.java
+++ src/progra2/s7/lab7/PSNUsers.java
@@ -11,6 +11,10 @@ package progra2.s7.lab7;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+
+import java.time.LocalDate;
 
 public class PSNUsers {
     
@@ -28,6 +32,64 @@ public class PSNUsers {
         users = new HashTable();
         //reloadHashTable();
     }
+    
+    private void reloadHashTable() throws IOException {
+        
+        users = new HashTable();
+        archivo.seek(0);
+        
+        try {
+            while (true) {
+                
+                String username = archivo.readUTF();
+                long posPuntos = archivo.getFilePointer();
+                int puntos = archivo.readInt();
+                int contadorTrofeos = archivo.readInt();
+                boolean activo = archivo.readBoolean();
+
+                if (activo) {
+                    users.add(username, posPuntos);
+                }
+            }
+        } catch (EOFException e) {
+            System.out.println("Lectura completada (EOF)");
+        }
+        
+    }
+    
+    public boolean addUser(String username) throws IOException {
+        
+        if (users.search(username) != -1) {
+            return false;
+        }
+
+        archivo.seek(archivo.length());
+        archivo.writeUTF(username);
+        long posPuntos = archivo.getFilePointer();
+        archivo.writeInt(0);
+        archivo.writeInt(0);
+        archivo.writeBoolean(true);
+
+        users.add(username, posPuntos);
+        return true;
+    }
+    
+    public boolean deactivateUser(String username) throws IOException {
+        
+        long posPuntos = users.search(username);
+        if (posPuntos == -1) {
+            return false;
+        }
+        
+        long posActivo = posPuntos + 4 + 4;
+        archivo.seek(posActivo);
+        archivo.writeBoolean(false);
+
+        users.remove(username);
+        return true;
+    }
+    
+    
     
     
 }
